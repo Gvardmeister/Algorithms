c48b5c16a1deeb38e77362324af475f82f9513fd
Gvardmeister <alexgvardin@gmail.com>
2026-01-08 07:15:17 +0300

    add three_sum

--- two_pointers/two_pointers_medium/three_sum.go
+++ two_pointers/two_pointers_medium/three_sum.go
@@ -1,8 +1,10 @@
 package two_pointers_medium
 
+import "sort"
+
 /*
-	Лучшая структура это -
-	Краевые случаи:
+	Лучшая структура это - slice
+	Краевые случаи: массив не может быть пустым
 	Сложность:
 
 	Задан целочисленный массив nums.
@@ -23,5 +25,45 @@ package two_pointers_medium
 */
 
 func threeSum(nums []int) [][]int {
-	return nil
+	var result [][]int // альтернатива result := [][]int{}
+
+	if len(nums) < 3 {
+		return result
+	}
+
+	sort.Ints(nums)
+
+	for i := 0; i < len(nums)-2; i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+
+		left := i + 1
+		right := len(nums) - 1
+
+		for left < right {
+			sum := nums[i] + nums[left] + nums[right]
+
+			if sum == 0 {
+				result = append(result, []int{nums[i], nums[left], nums[right]})
+
+				for left < right && nums[left] == nums[left+1] {
+					left++
+				}
+
+				for left < right && nums[right] == nums[right-1] {
+					right--
+				}
+
+				left++
+				right--
+			} else if sum < 0 {
+				left++
+			} else {
+				right--
+			}
+		}
+	}
+
+	return result
 }
